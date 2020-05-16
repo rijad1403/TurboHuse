84d0520df6f78150182b24e37228d24493a63e33
Rijad Gračanin <rijad.gracanin14@gmail.com>
2020-05-16 13:03:37 +0200

    Fixed css link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
-    <link rel="stylesheet" href=" {{asset('css/app.css')}} ">
+    <link rel="stylesheet" href=" {{secure_asset('./css/app.css')}} ">
     <title>TurboHuse</title>
 </head>
 
