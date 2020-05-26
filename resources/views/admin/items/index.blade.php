0a906fc7056411a11ff4ea5c4379779d10589968
Rijad Gračanin <rijad.gracanin14@gmail.com>
2020-05-26 10:45:47 +0200

    fix

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -19,113 +19,112 @@
                     proizvođača</button>
             </div>
             @endif
-            <form action="/artikli/search" method="GET">
+            {{-- <form action="/artikli/search" method="GET">
                 <div class="form-group">
                     <label for="car-select"><i class="fas fa-car"></i> Proizvođač</label>
                     <select class="form-control" id="car-select" name="car">
                         <option value="">Izaberite proizvođača</option>
                         @forelse ($manufacturers as $manufacturer)
                         <option value={{ $manufacturer->id }}>{{ $manufacturer->title }}</option>
-                        @empty
-                        <option value="">Nema unešenih proizvođača...</option>
-                        @endforelse
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="price"><i class="fas fa-coins"></i> Cijena (KM)</label>
-                    <input type="text" name="maxPrice" id="price" class="form-control"
-                        placeholder="Maksimalna cijena, npr. 1000">
-                </div>
-                <div class="form-group">
-                    <label for="releaseYear"><i class="fas fa-calendar"></i> Godina proizvodnje</label>
-                    <input type="text" name="releaseYear" id="releaseYear" class="form-control" placeholder="Npr. 2005">
-                </div>
-
-                <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Pretraga artikala</button>
-            </form>
+            @empty
+            <option value="">Nema unešenih proizvođača...</option>
+            @endforelse
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="price"><i class="fas fa-coins"></i> Cijena (KM)</label>
+            <input type="text" name="maxPrice" id="price" class="form-control"
+                placeholder="Maksimalna cijena, npr. 1000">
+        </div>
+        <div class="form-group">
+            <label for="releaseYear"><i class="fas fa-calendar"></i> Godina proizvodnje</label>
+            <input type="text" name="releaseYear" id="releaseYear" class="form-control" placeholder="Npr. 2005">
         </div>
 
-        <div class="col-sm-12 col-md-12 col-lg-9">
-            <div class="row">
-                <div class="col-lg-4">
-                    @if (session('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ session('success') }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @elseif (session('warning'))
-                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                        {{ session('warning') }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @elseif ($errors->any())
-                    @foreach ($errors->all() as $error)
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $error }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @endforeach
-                    @endif
-                    @if ($filterMessage != '')
-                    <div class="alert alert-light" role="alert">
-                        {{ $filterMessage }}
-                    </div>
-                    @endif
-                </div>
-            </div>
-            <div class="row">
-                @forelse ($items as $item)
-                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                    <div class="card item" style="width: 80%;">
-                        <img class="card-img-top" src=" {{ $item->images[0]->title }} " alt="Card image cap">
-                        <div class="card-body">
-                            <small class="text-muted">{{$item->manufacturer->title}}</small>
-                            <h5 class="card-title">{{$item->title}}</h5>
-                            <h5 class="card-title">{{$item->price}} KM</h5>
+        <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Pretraga artikala</button>
+        </form> --}}
+    </div>
 
-                            @if (!Auth::user() || Auth::user()->type !== 'admin' )
-                            <div>
-                                <button class="btn btn-primary" onclick="addToCart({{ $item }})">
-                                    <i class="fas fa-cart-plus"></i> Dodaj u košaru
-                                </button>
-                            </div>
-                            @endif
-                            <div>
-                                <a class="btn btn-primary" href="/artikli/{{$item->id}}" role="button"
-                                    style="text-transform: initial; border-radius: 0;"><i
-                                        class="fas fa-info-circle"></i>
-                                    Opširnije</a>
-                            </div>
-                            @if (Auth::user() && Auth::user()->type == "admin")
-                            <div>
-                                <a class="btn btn-primary" href="/artikli/{{$item->id}}/uredi" role="button"
-                                    style="text-transform: initial; border-radius: 0;"><i class="fas fa-edit"></i>
-                                    Uredi artikl</a> <br>
-                                <button class="btn btn-primary" data-toggle="modal"
-                                    data-target="#deleteItemModal{{ $item->id }}" title="Ukloni artikl"><i
-                                        class="far fa-trash-alt"></i> Ukloni artikl
-                                </button>
-                            </div>
-                            @endif
+    <div class="col-sm-12 col-md-12 col-lg-9">
+        <div class="row">
+            <div class="col-lg-4">
+                @if (session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @elseif (session('warning'))
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    {{ session('warning') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @elseif ($errors->any())
+                @foreach ($errors->all() as $error)
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ $error }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endforeach
+                @endif
+                @if ($filterMessage != '')
+                <div class="alert alert-light" role="alert">
+                    {{ $filterMessage }}
+                </div>
+                @endif
+            </div>
+        </div>
+        <div class="row">
+            @forelse ($items as $item)
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                <div class="card item" style="width: 80%;">
+                    <img class="card-img-top" src=" {{ $item->images[0]->title }} " alt="Card image cap">
+                    <div class="card-body">
+                        <small class="text-muted">{{$item->manufacturer->title}}</small>
+                        <h5 class="card-title">{{$item->title}}</h5>
+                        <h5 class="card-title">{{$item->price}} KM</h5>
+
+                        @if (!Auth::user() || Auth::user()->type !== 'admin' )
+                        <div>
+                            <button class="btn btn-primary" onclick="addToCart({{ $item }})">
+                                <i class="fas fa-cart-plus"></i> Dodaj u košaru
+                            </button>
                         </div>
+                        @endif
+                        <div>
+                            <a class="btn btn-primary" href="/artikli/{{$item->id}}" role="button"
+                                style="text-transform: initial; border-radius: 0;"><i class="fas fa-info-circle"></i>
+                                Opširnije</a>
+                        </div>
+                        @if (Auth::user() && Auth::user()->type == "admin")
+                        <div>
+                            <a class="btn btn-primary" href="/artikli/{{$item->id}}/uredi" role="button"
+                                style="text-transform: initial; border-radius: 0;"><i class="fas fa-edit"></i>
+                                Uredi artikl</a> <br>
+                            <button class="btn btn-primary" data-toggle="modal"
+                                data-target="#deleteItemModal{{ $item->id }}" title="Ukloni artikl"><i
+                                    class="far fa-trash-alt"></i> Ukloni artikl
+                            </button>
+                        </div>
+                        @endif
                     </div>
                 </div>
-                @empty
-                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-                    <p>
-                        Nema unešenih artikala...
-                    </p>
-                </div>
-                @endforelse
             </div>
+            @empty
+            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                <p>
+                    Nema unešenih artikala...
+                </p>
+            </div>
+            @endforelse
         </div>
     </div>
+</div>
 </div>
 
 <div class="modal fade" id="newItem" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
